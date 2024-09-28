625d86134cca8aaa9b33f7536f54b7fb43fc270f
Cupcake4fia <56863364+SOFFIYAH@users.noreply.github.com>
2024-09-28 08:40:45 +0100

    eight commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter, Routes, Route} from "react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  
-    <App />
+  <BrowserRouter basename={'/tip-calculator/'}>
+  <Route exact path="/" component={Index} />
+</BrowserRouter>
+<Route path="*" element={<App />}>
+</Routes>
+    
   </React.StrictMode>,
 )
