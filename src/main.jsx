dcccc895846c55ffbd98eafec6fa414d447e0ff0
Cupcake4fia <56863364+SOFFIYAH@users.noreply.github.com>
2024-09-28 09:47:39 +0100

    11th commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter, Routes, Route} from "react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter basename={'/tip-calculator/'}>
+  <BrowserRouter basename={impott.meta.env.BASE_URL}>
   
 <Routes>
 <Route path="*" element={<App />}/>
